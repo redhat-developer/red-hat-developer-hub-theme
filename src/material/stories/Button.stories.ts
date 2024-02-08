82246645eec9537919a21847edf5990689fe4478
Christoph Jerolimov <jerolimov+git@redhat.com>
2024-02-08 08:46:45 +0100

    🎨 replace custom Button with material-ui Button

--- src/material/stories/Button.stories.ts
+++ src/material/stories/Button.stories.ts
@@ -1,50 +1,47 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import Button from "@mui/material/Button";
+import type { Meta as MetaObj, StoryObj } from "@storybook/react";
 
-import { Button } from "./Button";
+type Meta = MetaObj<typeof Button>;
+type Story = StoryObj<MetaObj>;
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: "Example/Button",
+const meta: Meta = {
+  title: "MUI/Button",
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
+    onClick: { action: "clicked" },
   },
-} satisfies Meta<typeof Button>;
+};
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: "Button",
+    color: "primary",
+    children: "Button",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: "Button",
+    color: "secondary",
+    children: "Button",
   },
 };
 
 export const Large: Story = {
   args: {
     size: "large",
-    label: "Button",
+    children: "Button",
   },
 };
 
 export const Small: Story = {
   args: {
     size: "small",
-    label: "Button",
+    children: "Button",
   },
 };
--- src/material/stories/Header.tsx
+++ src/material/stories/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
-import { Button } from "./Button";
 import "./header.css";
 
 type User = {
@@ -52,17 +52,18 @@ export const Header = ({
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <Button size="small" onClick={onLogout}>
+              Log out
+            </Button>
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button
-              primary
-              size="small"
-              onClick={onCreateAccount}
-              label="Sign up"
-            />
+            <Button size="small" onClick={onLogin}>
+              Log in
+            </Button>
+            <Button color="primary" size="small" onClick={onCreateAccount}>
+              Sign up
+            </Button>
           </>
         )}
       </div>
