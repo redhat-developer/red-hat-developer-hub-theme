68b448ccf0dab2dadea864c4a4bc8f8c1459dcfe
Christoph Jerolimov <jerolimov+git@redhat.com>
2024-02-08 14:46:50 +0100

    Updates themes

--- src/mui/patternfly-4-dark-theme/index.ts
+++ src/mui/patternfly-4-dark-theme/index.ts
@@ -6,10 +6,52 @@ export const themeOptions: ThemeOptions = {
     mode: "dark",
     primary: brown,
   },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+  },
   components: {
     MuiButton: {
       defaultProps: {
         disableRipple: true,
+      },
+    },
+    MuiTabs: {
+      defaultProps: {
+        sx: {
+          // borderTop: '1px solid red',
+        },
+        TabIndicatorProps: {
+          style: {
+            height: "3px",
+            backgroundColor: "red",
+          },
+        },
+      },
+      styleOverrides: {
+        root: {
+          // border: solid var(--pf-v5-c-tabs--before--BorderColor);
+          borderBottom: "1px solid #d2d2d2",
+        },
+      },
+    },
+    MuiTab: {
+      defaultProps: {
+        disableRipple: true,
+      },
+      styleOverrides: {
+        root: {
+          // backgroundColor: 'gray',
+          minWidth: "auto",
+          padding: "16px 8px",
+        },
+        // selected: {
+        //   backgroundColor: 'lightred',
+        // },
+        disabled: {
+          backgroundColor: "gray",
+        },
       },
     },
   },
