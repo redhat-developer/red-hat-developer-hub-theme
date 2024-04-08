fcdf0d5986f01f2b24b0c6a3c03b99ba495e528f
Christoph Jerolimov <jerolimov+git@redhat.com>
2024-04-09 00:00:08 +0200

    Export themes

--- src/themes/index.tsx
+++ src/themes/index.tsx
@@ -32,69 +32,86 @@ const createProvider =
     );
   };
 
-const devAppThemes: AppTheme[] = [
-  {
-    id: "backstage-light",
-    title: "Backstage Light",
-    variant: "light",
-    icon: <LightIcon />,
-    Provider: createProvider(themes.light),
-  },
-  {
-    id: "backstage-dark",
-    title: "Backstage Dark",
-    variant: "dark",
-    icon: <DarkIcon />,
-    Provider: createProvider(themes.dark),
-  },
-  {
-    id: "rhdh-1.0-light",
-    title: "RHDH 1.0 Light",
-    variant: "light",
-    icon: <LightIcon />,
-    Provider: createProvider(rhdh10.customLightTheme({})),
-  },
-  {
-    id: "rhdh-1.0-dark",
-    title: "RHDH 1.0 Dark",
-    variant: "dark",
-    icon: <DarkIcon />,
-    Provider: createProvider(rhdh10.customDarkTheme({})),
-  },
-  {
-    id: "rhdh-1.1-light",
-    title: "RHDH 1.1 Light",
-    variant: "light",
-    icon: <LightIcon />,
-    Provider: createProvider(rhdh11.customLightTheme({})),
-  },
-  {
-    id: "rhdh-1.1-dark",
-    title: "RHDH 1.1 Dark",
-    variant: "dark",
-    icon: <DarkIcon />,
-    Provider: createProvider(rhdh11.customDarkTheme({})),
-  },
-  {
-    id: "rhdh-1.2-light",
-    title: "RHDH 1.2 Light",
-    variant: "light",
-    icon: <LightIcon />,
-    Provider: createProvider(rhdh12.customLightTheme({})),
-  },
-  {
-    id: "rhdh-1.2-dark",
-    title: "RHDH 1.2 Dark",
-    variant: "dark",
-    icon: <DarkIcon />,
-    Provider: createProvider(rhdh12.customDarkTheme({})),
-  },
-];
-
 export const createDevAppThemes = () => {
-  return devAppThemes;
+  return [
+    {
+      id: "backstage-light",
+      title: "Backstage Light",
+      variant: "light",
+      icon: <LightIcon />,
+      Provider: createProvider(themes.light),
+    },
+    {
+      id: "backstage-dark",
+      title: "Backstage Dark",
+      variant: "dark",
+      icon: <DarkIcon />,
+      Provider: createProvider(themes.dark),
+    },
+    {
+      id: "rhdh-1.0-light",
+      title: "RHDH 1.0 Light",
+      variant: "light",
+      icon: <LightIcon />,
+      Provider: createProvider(rhdh10.customLightTheme({})),
+    },
+    {
+      id: "rhdh-1.0-dark",
+      title: "RHDH 1.0 Dark",
+      variant: "dark",
+      icon: <DarkIcon />,
+      Provider: createProvider(rhdh10.customDarkTheme({})),
+    },
+    {
+      id: "rhdh-1.1-light",
+      title: "RHDH 1.1 Light",
+      variant: "light",
+      icon: <LightIcon />,
+      Provider: createProvider(rhdh11.customLightTheme({})),
+    },
+    {
+      id: "rhdh-1.1-dark",
+      title: "RHDH 1.1 Dark",
+      variant: "dark",
+      icon: <DarkIcon />,
+      Provider: createProvider(rhdh11.customDarkTheme({})),
+    },
+    {
+      id: "rhdh-1.2-light",
+      title: "RHDH 1.2 Light",
+      variant: "light",
+      icon: <LightIcon />,
+      Provider: createProvider(rhdh12.customLightTheme({})),
+    },
+    {
+      id: "rhdh-1.2-dark",
+      title: "RHDH 1.2 Dark",
+      variant: "dark",
+      icon: <DarkIcon />,
+      Provider: createProvider(rhdh12.customDarkTheme({})),
+    },
+  ];
+};
+
+export const useAllThemes = () => {
+  return React.useMemo(() => createDevAppThemes(), []);
 };
 
 export const useThemes = () => {
-  return React.useMemo(() => devAppThemes, []);
+  return React.useMemo(() => [
+    {
+      id: "light",
+      title: "Light",
+      variant: "light",
+      icon: <LightIcon />,
+      Provider: createProvider(rhdh12.customLightTheme({})),
+    },
+    {
+      id: "dark",
+      title: "Dark",
+      variant: "dark",
+      icon: <DarkIcon />,
+      Provider: createProvider(rhdh12.customDarkTheme({})),
+    },
+  ], []);
 };
