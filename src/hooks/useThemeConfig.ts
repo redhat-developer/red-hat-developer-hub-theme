52a90e5db1113060fe48ba17f1b1d819da55f5f3
Yi Cai <yicai@redhat.com>
2024-09-24 17:49:56 -0400

    Fixed lint issue
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- src/hooks/useThemeConfig.ts
+++ src/hooks/useThemeConfig.ts
@@ -22,10 +22,12 @@ export const useThemeConfig = (themeName: string): ThemeConfig => {
       themeConfig.mode = themeName.includes("dark") ? "dark" : "light";
     }
 
-    if (themeConfig) {
-      if (Array.isArray(themeConfig.pageTheme?.default.fontColor)) {
-        themeConfig.pageTheme.default.fontColor = themeConfig.pageTheme.default.fontColor[0];
-      }
+    if (
+      themeConfig &&
+      Array.isArray(themeConfig.pageTheme?.default.fontColor)
+    ) {
+      themeConfig.pageTheme.default.fontColor =
+        themeConfig.pageTheme.default.fontColor[0];
     }
 
     return themeConfig;
