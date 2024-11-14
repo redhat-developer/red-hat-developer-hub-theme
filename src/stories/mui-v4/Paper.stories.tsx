797eb2b05633c45e815eb712e93c36b5e81c1e13
Christoph Jerolimov <jerolimov+git@redhat.com>
2024-11-14 17:28:48 +0100

    Add new stories to test paper component

---
+++ src/stories/mui-v4/Paper.stories.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import type { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "MUI-v4/Paper",
+} satisfies Meta;
+
+export const Papers: StoryObj = {
+  render: () => {
+    // elevations from 0 to 24
+    const elevations = Array.from({ length: 25 }, (_, i) => i);
+
+    return (
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {elevations.map((elevation) => (
+          <div key={elevation} style={{ margin: "20px" }}>
+            <Paper elevation={elevation}>
+              <div
+                style={{
+                  display: "flex",
+                  width: "100px",
+                  height: "100px",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                e={elevation}
+              </div>
+            </Paper>
+          </div>
+        ))}
+      </div>
+    );
+  },
+};
